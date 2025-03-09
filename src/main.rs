fc35d9f3a51b883817621f66552f3a6daefbb08a
Valentin Allmnd <valentin.allmnd@gmail.com>
2025-03-09 13:20:28 +0100

    adding config route

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,7 @@ use axum_keycloak_auth::{
 use charizhard_ota::route::{root, specific_firmware};
 use minio_rsc::{provider::StaticProvider, Minio};
 use reqwest::Url;
-use route::{delete_firmware, fallback, handle_manifest, latest_firmware, post_firmware};
+use route::{delete_firmware, fallback, handle_manifest, latest_firmware, post_firmware,config_wg};
 use std::{env, result::Result::Ok};
 mod route;
 
@@ -23,20 +23,28 @@ pub struct MinioInstance {
 impl MinioInstance {
     pub fn new() -> Result<MinioInstance, anyhow::Error> {
         // setup database config
+        println!("Minio: Attempting to load configuration from environment");
+        let access_key = env::var("MINIO_SECRET_KEY").unwrap_or_else(|_| "Not Set".to_string());
+        let secret_key = env::var("MINIO_ACCESS_KEY").unwrap_or_else(|_| "Not Set".to_string());
+        println!("Debug - MINIO_ACCESS_KEY: {}", access_key);
+        println!("Debug - MINIO_SECRET_KEY: {}", secret_key);
         let provider = match StaticProvider::from_env() {
             Some(provider) => provider,
             None => return Err(Error::msg("Env varibles not found")),
         };
 
+        println!("Minio: Successfully loaded configuration from environment");
         //let ip_addr = env::var("IP_MINIO")?;
         //let port = env::var("PORT_MINIO")?;
         //let endpoint = ip_addr + ":" + &port;
 
+        println!("Minio: Attempting to connect to database at localhost:9000");
         let minio = Minio::builder()
-            .endpoint("minio-service.minio.svc.cluster.local:9000") //where to look for database
+            .endpoint("localhost:9000") //where to look for database
             .provider(provider)
             .secure(false)
             .build()?;
+        println!("Minio: Successfully connected to database at localhost:9000");
         Ok(MinioInstance { minio })
     }
 
@@ -45,12 +53,14 @@ impl MinioInstance {
     }
 }
 
+
 pub fn public_router(instance: MinioInstance) -> Router {
     Router::new()
         .route("/", get(root))
         .route("/latest", get(latest_firmware))
         .route("/firmware/{file_name}", get(specific_firmware))
         .route("/manifest", get(handle_manifest))
+        .route("/configwg", get(config_wg))//TODO : mettre dans le eprotected_router pour tester
         .with_state(instance.get_minio())
 }
 
@@ -66,8 +76,9 @@ pub fn protected_router(instance: KeycloakAuthInstance, minstance: MinioInstance
                 .passthrough_mode(PassthroughMode::Block)
                 .persist_raw_claims(false)
                 .expected_audiences(vec![String::from("account")])
-                .required_roles(vec![String::from("Admin")])
-                .build(),
+                .required_roles(vec![String::from("admin")])
+                .build()
+                
         )
         .with_state(minstance.get_minio())
 }
@@ -84,18 +95,19 @@ async fn main() -> Result<(), Error> {
     let keycloak_auth_instance = KeycloakAuthInstance::new(
         KeycloakConfig::builder()
             // a modifier évidemment au deployement
-            .server(Url::parse("http://keycloak-service.keycloak.svc.cluster.local:8080").unwrap())
+            .server(Url::parse("http://localhost:8080").unwrap())
             .realm(String::from("charizhard-ota"))
             .build(),
     );
 
     let minstance = MinioInstance::new()?;
     let router = public_router(minstance.clone())
+    // let router = public_router();
         .merge(protected_router(keycloak_auth_instance, minstance.clone()))
         .fallback(fallback);
 
     // 0.0.0.0 signifie qu'on écoute sur toutes les nci
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:8081").await?;
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:8082").await?;
     axum::serve(listener, router.into_make_service()).await?;
     Ok(())
 }
--- src/route/mod.rs
+++ src/route/mod.rs
@@ -8,10 +8,11 @@ use axum::{
     Json,
 };
 use minio_rsc::{client::ListObjectsArgs, Minio};
+use crate::route::utils::get_file;
 use regex::Regex;
 use reqwest::Method;
 use serde::Serialize;
-use utils::get_file;
+use utils::parse_client_json;
 mod utils;
 
 const FIRMWARE_DIR: &str = "bin";
@@ -128,9 +129,9 @@ pub async fn specific_firmware(
     return get_file(instance, &file_name).await;
 }
 
-//curl -X POST http://localhost:8080/firmware/charizhard.V1.3.bin \
-//  -T ./firmware.bin \
-//  -H "Authorization: Bearer $JWT_TOKEN"
+// //curl -X POST http://localhost:8080/firmware/charizhard.V1.3.bin \
+// //  -T ./firmware.bin \
+// //  -H "Authorization: Bearer $JWT_TOKEN"
 pub async fn post_firmware(
     AxumPath(file_name): AxumPath<String>,
     State(instance): State<Minio>,
@@ -188,3 +189,90 @@ pub async fn delete_firmware(
 pub async fn fallback() -> (StatusCode, &'static str) {
     (StatusCode::NOT_FOUND, "Not Found")
 }
+
+
+/// Handles the WireGuard configuration request by retrieving the configuration
+/// file from the "config-wg" bucket in Minio and parsing it based on the
+/// `id_client_x` header from the request. Returns the parsed configuration or
+/// an error message if the operation fails.
+/// 
+/// # Arguments
+/// 
+/// * `State(instance)`: Minio instance used to interact with the object storage.
+/// * `req`: The HTTP request containing headers with client information.
+/// 
+/// # Returns
+/// 
+/// An HTTP response indicating the status of the operation:
+/// * `StatusCode::OK` with "LOADING CONFIG" if the configuration is successfully loaded.
+/// * `StatusCode::NO_CONTENT` if no files are found in the bucket.
+/// * `StatusCode::INTERNAL_SERVER_ERROR` with an error message if an error occurs.
+/// 
+/// # Errors
+/// 
+/// Returns an error if:
+/// * The `id_client_x` header is missing or invalid.
+/// * There is an error querying the bucket or retrieving the file.
+/// * The configuration cannot be parsed successfully.
+
+#[axum::debug_handler]
+pub async fn config_wg(State(instance): State<Minio>,req: Request) -> impl IntoResponse {
+    // println!("hello endpoint reached");
+    let args = ListObjectsArgs::default();
+    let query = instance.list_objects("config-wg", args).await;
+    let headers = req.headers();
+    // println!("Headers: {:#?}", headers.get("id_client_x"));
+    let mut id_client_x_value = "";
+    let mut config = None;
+    if let Some(id_client_x) = headers.get("id_client_x") {
+        id_client_x_value = id_client_x.to_str().unwrap();
+        // println!("{:?}",id_client_x_value);
+    } else {
+        println!("Bad header");
+        return (
+            StatusCode::INTERNAL_SERVER_ERROR,
+            "Bad header".to_string(),
+        )
+    }
+    match query {
+        Ok(res) => {
+            let file_list: Vec<String> = res
+                .contents
+                .iter()
+                .map(|object| object.key.clone()) 
+                .collect();
+            let file = instance.get_object("config-wg",&file_list[0]).await;
+            match file {
+                Ok(response) => {
+                    let contents = response.text().await.unwrap();
+                    // println!("Contenu du fichier : {}", contents);
+                    config = parse_client_json(&contents, &id_client_x_value);
+                    println!("Config : {:?}", config);
+                }
+                Err(e) => {
+                    println!("Erreur lors de la récupération du contenu du fichier : {}", e);
+                }
+            }
+            // for file in &file_list {
+            //     println!("Fichier trouvé : {}", file);
+            // }
+
+            if file_list.is_empty() {
+                return (
+                    StatusCode::NO_CONTENT,
+                    "No Files Found in Bucket".to_string(),
+                );
+            }
+
+            (
+                StatusCode::OK,
+                "LOADING CONFIG".to_string(),//TODO à modif pour renvoyer la config une fois le tls/mtls ok
+            )
+        }
+        Err(e) => (
+            StatusCode::INTERNAL_SERVER_ERROR,
+            format!("Error deleting firmware {}", e)
+        ),
+    }
+}
+
